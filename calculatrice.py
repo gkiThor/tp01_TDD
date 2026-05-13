c5f1f9862febbfa00c546cdf0b9a5afdf9ec1705
GuillaumeSOULICHANH <g.soulichanh@gmail.com>
2026-05-13 11:46:47 +0200

    update calculatrice

--- calculatrice.py
+++ calculatrice.py
@@ -8,25 +8,24 @@ def multiplier(a, b):
     return a * b
 
 def diviser(a, b):
-    try:
-        if b == 0:
-            raise ValueError("Pas divisible par 0")
-
-        resultat = a / b
-        print(resultat)
-        return resultat
-
-    except ValueError as e:
-        print(e)
+    if b == 0:
+        raise ValueError("Pas divisible par 0")
+    return a / b
 
 def carre(nombre):
     return nombre * nombre
+
+def modulo(a, b):
+    return a % b
+
+def puissance(a, b):
+    return a ** b
 
 if __name__ == "__main__":
     print("Addition      : 2 + 3 =", additionner(2, 3))
     print("Soustraction  : 10 - 4 =", soustraire(10, 4))
     print("Multiplication: 3 x 4 =", multiplier(3, 4))
     print("Division      : 10 / 2 =", diviser(10, 2))
-    print("Division      : 10 / 0 =", diviser(10, 0))
-    print("Carre          :5 * 5 =",carre(5))
-   
+    print("Carre         : 5 * 5 =", carre(5))
+    print("Modulo        : 10 % 3 =", modulo(10, 3))
+    print("Puissance     : 2 ^ 3 =", puissance(2, 3))
